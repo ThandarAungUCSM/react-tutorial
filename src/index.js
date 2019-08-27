b7188e20801dbf92058fa08bd100db3fddc44f8e
ThandarAung <thandaraung903@gmail.com>
2019-08-27 18:43:17 +0900

    JSX to JavaScript and Back

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <h1>Hello!</h1>
-// function App() {
-//     return (
-//         <h1>Hello World</h1>
-//     )
-// }
+// const App = () => <h1>Hello!</h1>
+function App() {
+    const firstName = "Thandar"
+    const secondName = "Aung"
+
+    const date = new Date()
+    const hours = date.getHours()
+    let timeofDay
+
+    if (hours < 12) {
+        timeofDay = "Morning"
+    } else if (hours >= 12 && hours <17) {
+        timeofDay = "afternoon"
+    } else {
+        timeofDay = "Night"
+    }
+
+    return (
+        <div>
+            {/* <h1>Hello {firstName + " " + secondName }!</h1> */}
+            <h1>Hello { `${firstName} ${secondName}`}!</h1>
+
+            <h1>It is currently about { hours % 12} o'clock! </h1>
+            <h1>Good { timeofDay }</h1>
+        </div>
+    )
+}
 
 // JSX
 ReactDOM.render(
