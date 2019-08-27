98ba975332f835f527a3f6851d40795022281ca1
ThandarAung <thandaraung903@gmail.com>
2019-08-27 17:38:17 +0900

    Move Components into Separate Files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function MyApp() {
-    return (
-        <div>
-            <h1>TDA - UCSM</h1>
-            <p> This is a paragraph about me...</p>
-            <ul>
-                <li>Thailand</li>
-                <li>Japan</li>
-                <li>Singapore</li>
-            </ul>
-        </div>
-    )
-}
+import MyInfo from './components/MyInfo'
 
 // JSX
 ReactDOM.render(
-    <MyApp />, 
+    <MyInfo />, 
     document.getElementById('root')
 );
