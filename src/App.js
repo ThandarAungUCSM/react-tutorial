f8c2fc4c34b1ae7525eccf9c5d90690d140d8baa
ThandarAung <thandaraung903@gmail.com>
2019-08-27 18:10:58 +0900

    Parent/Child Components Practice

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
 
+import Header from './components/Header'
+import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
-      <nav>
-        <h1>Hello a third time!</h1>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-        </ul>
-      </nav>
-      <main>
-        <p>This is where most of my content will go ... </p>
-      </main>
+      <Header />
+      <MainContent />
       <Footer />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <p>This is the Header element.</p>
+        </header>
+    )
+}
+
+export default Header
