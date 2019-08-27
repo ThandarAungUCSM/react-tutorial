4c90f2e86443ffbb56b98c09dcd4f64be1d36dcf
ThandarAung <thandaraung903@gmail.com>
2019-08-27 17:28:24 +0900

    Functional Components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function MyApp() {
+    return (
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
+
 // JSX
 ReactDOM.render(
-    <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-    </ul>, 
+    <MyApp />, 
     document.getElementById('root')
 );
