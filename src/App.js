5f81648f0bca13cd216850ac3a88470b29604999
ThandarAung <thandaraung903@gmail.com>
2019-08-28 13:20:10 +0900

    Mapping Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
 import Joke from './components/Joke'
-
+import jokesData from './JokesData'
 function App() {
+  const jokeComponents = jokesData.map( joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />)
+
+  // const nums = [1,2,3,4,5,6,7,8,9,10]
+  // const doubled = nums.map(function(num) {
+  //   return num * 2
+  // })
+  // console.log(doubled)
+
   return (
-    <div className="contacks">
-      <Joke
-        punchLine="My new job" />
-      <Joke 
-        question = "What's the name" 
-        punchLine="The name is Yu." />
-      <Joke 
-        question = "What's the city" 
-        punchLine="The city is Yangon." />
+    <div>
+      {jokeComponents}
     </div>
   );
 }
---
+++ src/JokesData.js
@@ -0,0 +1,28 @@
+const jokesData = [
+    {
+        id: 1,
+        punchLine: "It's hard to explain1"
+    },
+    {
+        id: 2,
+        punchLine: "It's hard to explain2",
+        question: "Question 2"
+    },
+    {
+        id: 3,
+        punchLine: "It's hard to explain3",
+        question: "Question 3"
+    },
+    {
+        id: 4,
+        punchLine: "It's hard to explain4",
+        question: "Question 4"
+    },
+    {
+        id: 5,
+        punchLine: "It's hard to explain5",
+        question: "Question 5"
+    }
+]
+
+export default jokesData
