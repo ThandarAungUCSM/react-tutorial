38ffaf39e181710418ccf996c3a9a63e8b29208b
ThandarAung <thandaraung903@gmail.com>
2019-08-28 12:12:50 +0900

    Props in React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
 
-import TodoItem from './components/TodoItem'
+import ContactCard from './components/ContactCard'
 
 function App() {
   return (
-    <div className="todo-list">
-      <TodoItem></TodoItem>
-      <TodoItem></TodoItem>
-      <TodoItem></TodoItem>
-      <TodoItem></TodoItem>
+    <div className="contacks">
+      <ContactCard 
+        contact = {{ name: "Mr. Whiskerson",
+                     imgUrl: "http://placekitten.com/360/200",
+                     phone: "(212) 555-1234",
+                     email: "mr.whiskaz@catnap.meow" }}
+      />
+
+      <ContactCard 
+        contact = {{ name: "Mr. Whiskerson",
+                     imgUrl: "http://placekitten.com/360/200",
+                     phone: "(212) 555-1234",
+                     email: "mr.whiskaz@catnap.meow" }}
+      />
+
+      <ContactCard 
+        contact = {{ name: "Mr. Whiskerson",
+                     imgUrl: "http://placekitten.com/360/200",
+                     phone: "(212) 555-1234",
+                     email: "mr.whiskaz@catnap.meow" }}
+      />
+
+      <ContactCard 
+        contact = {{ name: "Mr. Whiskerson",
+                     imgUrl: "http://placekitten.com/360/200",
+                     phone: "(212) 555-1234",
+                     email: "mr.whiskaz@catnap.meow" }}
+      />
+
     </div>
   );
 }
