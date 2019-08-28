9640396c3e4e95fec83ff07868e80a6e47f12b19
ThandarAung <thandaraung903@gmail.com>
2019-08-28 13:52:14 +0900

    Mapping Components Practice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 
-import Joke from './components/Joke'
-import jokesData from './JokesData'
+import Product from "./Product"
+import productsData from './vschoolProducts'
+
 function App() {
-  const jokeComponents = jokesData.map( joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />)
-
-  // const nums = [1,2,3,4,5,6,7,8,9,10]
-  // const doubled = nums.map(function(num) {
-  //   return num * 2
-  // })
-  // console.log(doubled)
-
+  const productComponents = productsData.map( item => <Product key = {item.id} product = {item} />)
   return (
     <div>
-      {jokeComponents}
+      { productComponents }
     </div>
   );
 }
---
+++ src/vschoolProducts.js
@@ -0,0 +1,22 @@
+const product = [
+    {
+        id: 1,
+        name: "Name 1",
+        price: 1000,
+        description: "Description 1"
+    },
+    {
+        id: 2,
+        name: "Name 2",
+        price: 2000,
+        description: "Description 2"
+    },
+    {
+        id: 3,
+        name: "Name 3",
+        price: 3000,
+        description: "Description 3"
+    }
+]
+
+export default product
